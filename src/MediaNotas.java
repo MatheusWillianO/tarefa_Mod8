46d56244d92d962edfdecb162759ce2a1b62668f
Matheus <matheusmail78@gmail.com>
2024-05-20 18:40:00 -0300

    first commit

---
+++ src/MediaNotas.java
@@ -0,0 +1,11 @@
+public class MediaNotas {
+    public static void main(String[] args) {
+        int nota1 = 10;
+        int nota2 = 7;
+        int nota3 = 5;
+        int nota4 = 8;
+        int notaFinal = nota1 + nota2 + nota3 + nota4;
+        int media = notaFinal / 4;
+        System.out.println(media);
+    }
+}
